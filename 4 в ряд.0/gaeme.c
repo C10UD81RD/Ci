e0513c82bb587ad5f8e332fb541cc44f7a7eb995
C10UD81RD <100081536+C10UD81RD@users.noreply.github.com>
2024-03-02 08:32:24 +0500

    Add files via upload

---
+++ 4 в ряд.0/gaeme.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define ROWS 10
+#define COLS 10
+
+void displayBoard(char board[ROWS][COLS])
+{
+    printf("1 2 3 4 5 6 7 8 9\n");
+    printf("_________________\n");
+    for (int i = ROWS - 1; i >= 1; i--)
+    {
+        for (int j = 1; j < COLS; j++)
+        {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("—————————————————\n");
+    printf("1 2 3 4 5 6 7 8 9\n");
+}
+
+// Функция для проверки, можно ли сделать ход в указанную колонку
+bool isValidMove(int col, char board[ROWS][COLS])
+{
+    if (col < 1 || col >= COLS)
+        return false;
+    if (board[ROWS - 1][col] != ' ')
+        return false;
+    return true;
+}
+
+// Функция для выпадения фишки в указанную колонку
+void makeMove(int col, char color, char board[ROWS][COLS])
+{
+    for (int i = 1; i < ROWS; i++)
+    {
+        if (board[i][col] == ' ')
+        {
+            board[i][col] = color;
+            break;
+        }
+    }
+}
+
+// Функция для проверки наличия победителя
+bool checkWin(char color, char board[ROWS][COLS])
+{
+    // Проверка горизонтальных линий
+    for (int i = 1; i < ROWS; i++)
+    {
+        for (int j = 1; j < COLS - 4; j++)
+        {
+            if (board[i][j] == color &&
+                board[i][j + 1] == color &&
+                board[i][j + 2] == color &&
+                board[i][j + 3] == color)
+                return true;
+        }
+    }
+
+    // Проверка вертикальных линий
+    for (int i = 1; i < ROWS - 4; i++)
+    {
+        for (int j = 1; j < COLS; j++)
+        {
+            if (board[i][j] == color &&
+                board[i + 1][j] == color &&
+                board[i + 2][j] == color &&
+                board[i + 3][j] == color)
+                return true;
+        }
+    }
+
+    // Проверка диагональных линий с положительным наклоном
+    for (int i = 1; i < ROWS - 4; i++)
+    {
+        for (int j = 1; j < COLS - 4; j++)
+        {
+            if (board[i][j] == color &&
+                board[i + 1][j + 1] == color &&
+                board[i + 2][j + 2] == color &&
+                board[i + 3][j + 3] == color)
+                return true;
+        }
+    }
+
+    // Проверка диагональных линий с отрицательным наклоном
+    for (int i = 4; i < ROWS; i++)
+    {
+        for (int j = 1; j < COLS - 4; j++)
+        {
+            if (board[i][j] == color &&
+                board[i - 1][j + 1] == color &&
+                board[i - 2][j + 2] == color &&
+                board[i - 3][j + 3] == color)
+                return true;
+        }
+    }
+
+    return false;
+}
+
+// Функция для игры
+void playGame()
+{
+    char board[ROWS][COLS];
+    for (int i = 1; i < ROWS; i++)
+    {
+        for (int j = 1; j < COLS; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
+
+    char currentPlayer = 'X';
+    bool gameOver = false;
+
+    while (!gameOver)
+    {
+        displayBoard(board);
+
+        printf("Ход игрока %c\n", currentPlayer);
+        int col;
+        printf("Введите номер колонки (от 1 до 9): ");
+        scanf("%d", &col);
+
+        if (isValidMove(col, board))
+        {
+            makeMove(col, currentPlayer, board);
+            system("clear");
+            if (checkWin(currentPlayer, board))
+            {
+                displayBoard(board);
+                printf("Вы победили!\n");
+                gameOver = true;
+            }
+            else
+            {
+                if (currentPlayer == 'X')
+                    currentPlayer = 'O';
+                else
+                    currentPlayer = 'X';
+            }
+        }
+        else
+        {
+            printf("Некорректный ход. Попробуйте ещё раз.\n");
+        }
+    }
+}
+
+void makeAIMove(char color, char board[ROWS][COLS])
+{
+    int col;
+    srand(time(NULL));
+    while (true)
+    {
+        col = rand() % (COLS - 1) + 1;
+        if (isValidMove(col, board))
+            break;
+    }
+    makeMove(col, color, board);
+}
+
+int main()
+{
+    char board[ROWS][COLS] = {' '};
+
+    char playerColor, aiColor;
+    printf("Выберите цвет фишек (X/O): ");
+    scanf("%c", &playerColor);
+    if (playerColor == 'X')
+        aiColor = 'O';
+    else
+        aiColor = 'X';
+
+    char currentPlayer = 'X';
+
+    while (true)
+    {
+        if (currentPlayer == playerColor)
+        {
+            int col;
+            printf("Ход игрока. Введите номер колонки (1-9): ");
+            scanf("%d", &col);
+            if (isValidMove(col, board))
+            {
+                makeMove(col, currentPlayer, board);
+                displayBoard(board);
+                if (checkWin(currentPlayer, board))
+                {
+                    printf("Игрок победил!\n");
+                    break;
+                }
+                currentPlayer = aiColor;
+            }
+            else
+            {
+                printf("Некорректный ход. Пожалуйста, повторите ввод.\n");
+            }
+        }
+        else
+        {
+            printf("Ход компьютера.\n");
+            makeAIMove(currentPlayer, board);
+            displayBoard(board);
+            if (checkWin(currentPlayer, board))
+            {
+                printf("Компьютер победил!\n");
+                break;
+            }
+            currentPlayer = playerColor;
+        }
+    }
+
+    return 0;
+}
+
+// Главная функция
+// int main()
+// {
+//     char choice;
+//     do
+//     {
+//         playGame();
+
+//         printf("--------\n");
+//         printf("Игра окончена.\n");
+//         printf("Нажмите 'M', чтобы вернуться в главное меню, или 'Q', чтобы выйти: ");
+//         scanf(" %c", &choice);
+//     } while (choice == 'M' || choice == 'm');
+
+//     return 0;
+// }
---
+++ 4врядъ/game.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROWS 10
+#define COLS 10
+
+void displayBoard(char board[ROWS][COLS])
+{
+    printf("1 2 3 4 5 6 7 8 9\n");
+    printf("_________________\n");
+    for (int i = ROWS - 1; i >= 1; i--)
+    {
+        for (int j = 1; j < COLS; j++)
+        {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("—————————————————\n");
+    printf("1 2 3 4 5 6 7 8 9\n");
+}
+
+bool isValidMove(int col, char board[ROWS][COLS])
+{
+    return (col >= 1 && col < COLS && board[ROWS - 1][col] == ' ');
+}
+
+void makeMove(int col, char color, char board[ROWS][COLS])
+{
+    for (int i = 1; i < ROWS; i++)
+    {
+        if (board[i][col] == ' ')
+        {
+            board[i][col] = color;
+            break;
+        }
+    }
+}
+
+bool checkWin(char color, char board[ROWS][COLS])
+{
+    // Проверка горизонтальных и вертикальных линий
+    for (int i = 1; i < ROWS; i++)
+    {
+        for (int j = 1; j < COLS - 3; j++)
+        {
+            if ((board[i][j] == color && board[i][j + 1] == color && board[i][j + 2] == color && board[i][j + 3] == color) ||
+                (board[j][i] == color && board[j + 1][i] == color && board[j + 2][i] == color && board[j + 3][i] == color))
+            {
+                return true;
+            }
+        }
+    }
+
+    // Проверка нисходящих диагоналей
+    for (int i = 1; i < ROWS - 3; i++)
+    {
+        for (int j = 1; j < COLS - 3; j++)
+        {
+            if (board[i][j] == color && board[i + 1][j + 1] == color && board[i + 2][j + 2] == color && board[i + 3][j + 3] == color)
+            {
+                return true;
+            }
+        }
+    }
+
+    // Проверка восходящих диагоналей
+    for (int i = 4; i < ROWS; i++)
+    {
+        for (int j = 1; j < COLS - 3; j++)
+        {
+            if (board[i][j] == color && board[i - 1][j + 1] == color && board[i - 2][j + 2] == color && board[i - 3][j + 3] == color)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    char board[ROWS][COLS];
+
+    // Инициализация доски
+    for (int i = 1; i < ROWS; i++)
+    {
+        for (int j = 1; j < COLS; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
+
+    srand(time(NULL));
+
+    bool isPlayerTurn = true;
+    char playerColor = '+';
+    char aiColor = '*';
+
+    while (true)
+    {
+        displayBoard(board);
+
+        if (isPlayerTurn)
+        {
+            // Ход игрока
+            int col;
+            printf("Введите номер колонки: ");
+            scanf("%d", &col);
+
+            if (isValidMove(col, board))
+            {
+                makeMove(col, playerColor, board);
+                if (checkWin(playerColor, board))
+                {
+                    printf("Вы победили!\n");
+                    break;
+                }
+                isPlayerTurn = false;
+            }
+            else
+            {
+                printf("Некорректный ход! Попробуйте снова.\n");
+            }
+        }
+        else
+        {
+            // Ход ИИ
+            int aiCol = rand() % (COLS - 1) + 1;
+
+            if (isValidMove(aiCol, board))
+            {
+                makeMove(aiCol, aiColor, board);
+                if (checkWin(aiColor, board))
+                {
+                    printf("ИИ победил!\n");
+                    break;
+                }
+                isPlayerTurn = true;
+            }
+        }
+    }
+
+    displayBoard(board);
+
+    return 0;
+}
